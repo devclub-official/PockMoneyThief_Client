fd5e0a906ced50af6b29dd7a405bf848bdda28a3
lecto17 <khwoo_rpsit@naver.com>
2025-12-04 13:58:00 +0900

    feat: 내 래플(참여, 주최, 당첨) 목록 조회 훅 추가

---
+++ src/hooks/useMyRaffles.ts
@@ -0,0 +1,24 @@
+import { myApi } from '@/lib/api'
+import { useSuspenseQueries } from '@tanstack/react-query'
+
+export const useMyRaffles = () => {
+	const [{ data: hostedRaffles }, { data: participatedRaffles }, { data: winRaffles }] =
+		useSuspenseQueries({
+			queries: [
+				{
+					queryKey: ['my-raffles'],
+					queryFn: () => myApi.getHostedRaffles(),
+				},
+				{
+					queryKey: ['my-participated-raffles'],
+					queryFn: () => myApi.getParticipatedRaffles(),
+				},
+				{
+					queryKey: ['my-wins'],
+					queryFn: () => myApi.getWins(),
+				},
+			],
+		})
+
+	return { hostedRaffles, participatedRaffles, winRaffles }
+}

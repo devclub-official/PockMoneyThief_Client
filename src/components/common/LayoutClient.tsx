af72c69ead45bb0988b11be000572e64ac09d9e0
kimhyein <70068498+henny1105@users.noreply.github.com>
2025-12-12 20:44:05 +0900

    chore: 로그인 페이지 헤더 숨김

--- src/components/common/LayoutClient.tsx
+++ src/components/common/LayoutClient.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from 'react'
+import { usePathname } from 'next/navigation'
 import { Header } from '@/components/common/Header'
 import { Sidebar } from '@/components/common/Sidebar'
 import { Footer } from '@/components/common/Footer'
@@ -11,6 +12,13 @@ interface LayoutClientProps {
 
 export function LayoutClient({ children }: LayoutClientProps) {
 	const [sidebarOpen, setSidebarOpen] = useState(false)
+	const pathname = usePathname()
+	const isLoginPage = pathname === '/login'
+
+	// 로그인 페이지에서는 헤더와 사이드바를 표시하지 않음
+	if (isLoginPage) {
+		return <>{children}</>
+	}
 
 	return (
 		<div className="flex h-screen overflow-hidden">
